177339becfa216abaf8def536efc10e5d84a18b2
annasolyannikova57 <60076564+stray322@users.noreply.github.com>
2025-03-31 11:18:25 +0400

    Update SortOrder.java

--- src/test/java/enums/SortOrder.java
+++ src/test/java/enums/SortOrder.java
@@ -1,1 +1,18 @@
-hibbjj
+package enums;
+
+import java.util.Comparator;
+
+public enum SortOrder {
+    ASCENDING(Comparator.naturalOrder()),
+    DESCENDING(Comparator.reverseOrder());
+
+    private final Comparator<String> comparator;
+
+    SortOrder(Comparator<String> comparator) {
+        this.comparator = comparator;
+    }
+
+    public Comparator<String> getComparator() {
+        return comparator;
+    }
+}
