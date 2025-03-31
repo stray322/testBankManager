091d06f738b939aec4f28d9cc62031d9ab734b58
annasolyannikova57 <60076564+stray322@users.noreply.github.com>
2025-03-31 10:57:49 +0400

    Update ManagerPage.java

--- src/main/java/pages/ManagerPage.java
+++ src/main/java/pages/ManagerPage.java
@@ -1,13 +1,16 @@
 package pages;
 
 import helpers.WebDriverHelper;
+import io.qameta.allure.Step;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
+
+/**
+ * Page Object для страницы менеджера банка.
+ * Позволяет добавлять новых клиентов через веб-интерфейс.
+ */
 
 public class ManagerPage extends BasePage {
 
@@ -17,7 +20,6 @@ public class ManagerPage extends BasePage {
     @FindBy(css = "input[ng-model='fName']")
     private WebElement firstNameInput;
 
-    // Добавлено поле для фамилии
     @FindBy(css = "input[ng-model='lName']")
     private WebElement lastNameInput;
 
@@ -27,23 +29,93 @@ public class ManagerPage extends BasePage {
     @FindBy(css = "button[type='submit']")
     private WebElement submitButton;
 
+    /**
+     * Конструктор класса.
+     * @param driver Экземпляр WebDriver.
+     */
+
     public ManagerPage(WebDriver driver) {
         super(driver);
     }
 
-    public void addCustomer(String firstName, String lastName, String postCode) {
-        WebDriverHelper.waitForClickable(driver, addCustomerButton, 10).click();
+    /**
+     * Добавляет нового клиента через UI.
+     * @param firstName Имя клиента.
+     * @param lastName Фамилия клиента.
+     * @param postCode Почтовый индекс.
+     * @return Текущий экземпляр ManagerPage.
+     */
+    @Step("Добавление клиента: {firstName} {lastName}, почтовый индекс: {postCode}")
+    public ManagerPage addCustomer(String firstName, String lastName, String postCode) {
+        return clickAddCustomerButton()
+                .enterFirstName(firstName)
+                .enterLastName(lastName)
+                .enterPostCode(postCode)
+                .submitForm()
+                .handleAlert();
+    }
 
-        WebDriverHelper.waitForVisibility(driver, firstNameInput, 5).sendKeys(firstName);
+    /**
+     * Нажимает кнопку добавления клиента.
+     * @return Текущий экземпляр ManagerPage.
+     */
+    @Step("Нажатие кнопки 'Add Customer'")
+    public ManagerPage clickAddCustomerButton() {
+        WebDriverHelper.waitForClickable(driver, addCustomerButton).click();
+        return this;
+    }
 
-        WebDriverHelper.waitForVisibility(driver, lastNameInput, 5).sendKeys(lastName);
+    /**
+     * Вводит имя клиента.
+     * @param firstName Имя клиента.
+     * @return Текущий экземпляр ManagerPage.
+     */
+    @Step("Ввод имени: {firstName}")
+    public ManagerPage enterFirstName(String firstName) {
+        WebDriverHelper.waitForVisibility(driver, firstNameInput).sendKeys(firstName);
+        return this;
+    }
 
-        WebDriverHelper.waitForVisibility(driver, postCodeInput, 5).sendKeys(postCode);
+    /**
+     * Вводит фамилию клиента.
+     * @param lastName Фамилия клиента.
+     * @return Текущий экземпляр ManagerPage.
+     */
+    @Step("Ввод фамилии: {lastName}")
+    public ManagerPage enterLastName(String lastName) {
+        WebDriverHelper.waitForVisibility(driver, lastNameInput).sendKeys(lastName);
+        return this;
+    }
 
-        WebDriverHelper.waitForClickable(driver, submitButton, 5).click();
+    /**
+     * Вводит почтовый индекс.
+     * @param postCode Почтовый индекс.
+     * @return Текущий экземпляр ManagerPage.
+     */
+    @Step("Ввод почтового индекса: {postCode}")
+    public ManagerPage enterPostCode(String postCode) {
+        WebDriverHelper.waitForVisibility(driver, postCodeInput).sendKeys(postCode);
+        return this;
+    }
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+    /**
+     * Отправляет форму.
+     * @return Текущий экземпляр ManagerPage.
+     */
+    @Step("Отправка формы")
+    public ManagerPage submitForm() {
+        WebDriverHelper.waitForClickable(driver, submitButton).click();
+        return this;
+    }
+
+    /**
+     * Обрабатывает всплывающее уведомление.
+     * @return Текущий экземпляр ManagerPage.
+     */
+    @Step("Обработка алерта")
+    public ManagerPage handleAlert() {
+        Alert alert = WebDriverHelper.waitForAlert(driver);
         alert.accept();
+        return this;
     }
 }
